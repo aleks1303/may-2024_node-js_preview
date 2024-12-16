11aba8e4df8c67733748de3e7ed0c3d6d00b9d6a
aleks1303 <aleksbulda13@gmail.com>
2024-12-16 13:47:09 +0200

    express

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = 5100;
+
+// звертаємось до app і задаємо, щоб вона приймала звернення
+app.get ('', () => {
+    console.log('Hello World!');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server has started on PORT ${PORT}` );
+})
+
+//На порту 5000 працює server
+//Браузер може робити тільки GET запити
