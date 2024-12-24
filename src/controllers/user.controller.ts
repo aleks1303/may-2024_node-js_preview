6fe4d465f4ae57c7ad05763ae9b3715b0bf62f97
aleks1303 <aleksbulda13@gmail.com>
2024-12-24 14:26:38 +0200

    6-router

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,9 +2,55 @@ import { Request, Response } from "express";
 
 import { users } from "../db/users.db";
 
+interface IUser {
+  name: string;
+  age: number;
+  gender: string;
+}
+
 class UserController {
-  public findAll(req: Request, res: Response) {
-    res.json(users);
+  public async findAll(
+    req: Request,
+    res: Response,
+  ): Promise<Response<IUser[]>> {
+    // return res.json(users);
+    // тут описуємо помилку
+    // тому що в потенційно-небезпечному коді це необхідно робити
+    try {
+    throw new Error("Something went wrong")
+    return res.json(users);
+    }
+    catch (e) {
+      return res.json({
+        message: e.message,
+        status: 400,
+      });
+  }
+  public async create(req: Request, res: Response) {
+    const user = req.body;
+    users.push(user);
+
+    res.status(201).json({ message: "Sponge Bob female created" });
+  }
+  public async updateById(req: Request, res: Response) {
+    console.log(req.params);
+    const { id } = req.params;
+    const updatedSpongeBobInfo = req.body;
+
+    users[+id] = updatedSpongeBobInfo;
+
+    res.status(200).json({
+      message: "Sponge Bob updated successfully",
+    });
+  }
+  public async deleteById(req: Request, res: Response) {
+    const { id } = req.params;
+
+    users.splice(+id, 1);
+
+    res.status(200).json({
+      message: "Sponge Bob deleted successfully",
+    });
   }
 }
 
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,7 +1,6 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 
 import { userController } from "../controllers/user.controller";
-import { users } from "../db/users.db";
 
 const router = Router();
 // router - не займається бізнес-логікою
@@ -11,36 +10,13 @@ const router = Router();
 router.get("/", userController.findAll);
 
 // робимо post request на sponge bob
-router.post("/", (req: Request, res: Response) => {
-  const user = req.body;
-  users.push(user);
-
-  res.status(201).json({ message: "Sponge Bob female created" });
-});
+router.post("/", userController.create);
 
 // оновлюємо одного з sponge по id
-router.put("/:id", (req: Request, res: Response) => {
-  console.log(req.params);
-  const { id } = req.params;
-  const updatedSpongeBobInfo = req.body;
-
-  users[+id] = updatedSpongeBobInfo;
-
-  res.status(200).json({
-    message: "Sponge Bob updated successfully",
-  });
-});
+router.put("/:id", userController.updateById);
 
 // видаляємо одного з sponge по id
 
-router.delete("/:id", (req: Request, res: Response) => {
-  const { id } = req.params;
-
-  users.splice(+id, 1);
-
-  res.status(200).json({
-    message: "Sponge Bob deleted successfully",
-  });
-});
+router.delete("/:id", userController.deleteById);
 
 export const userRouter = router;
